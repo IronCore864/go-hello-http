e2c5d28d75abd8f4a57c9cac0f12200734b0a062
Tiexin Guo <guotiexin@gmail.com>
2020-03-25 09:20:27 +0100

    update main to read name from env

--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/", helloServer)
+	log.Println("Server started on 8080.")
 	http.ListenAndServe(":8080", nil)
 }
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello!")
+func helloServer(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	if name == "" {
+		name = "world"
+	}
+	fmt.Fprintf(w, fmt.Sprintf("Hello, %s!", name))
 }
