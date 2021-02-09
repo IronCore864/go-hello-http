4ee743c52681873ec8e0f172a343320e2ab50f7a
Tiexin Guo <guotiexin@gmail.com>
2021-02-09 16:10:12 +0100

    update travis

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/ironcore864/go-hello-http
 
 go 1.15
-
-require (
-	github.com/aws/aws-lambda-go v1.22.0
-	github.com/epsagon/epsagon-go v1.23.0
-)
--- travis/docker_build_push.sh
+++ travis/docker_build_push.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-docker build -t ironcore864/go-hello-http .
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push ironcore864/go-hello-http
+VERSION=$(cat version)
+
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
+
+docker build -t public.ecr.aws/u7w2h8y6/go-hello-http:$VERSION .
+
+docker push public.ecr.aws/u7w2h8y6/go-hello-http:$VERSION
