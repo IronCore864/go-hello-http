5bbc06814de464268585457e356091248f2b0986
Tiexin Guo <guotiexin@gmail.com>
2021-02-09 17:53:09 +0100

    update

--- travis/docker_build_push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-VERSION=$(cat version)
-docker build -t $DOCKER_REPO:$VERSION .
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push $DOCKER_REPO:$VERSION
