f2e563dc1b493ca292e6d941d909ab906f2a1a46
Tiexin Guo <guotiexin@gmail.com>
2023-06-15 16:55:16 +0800

    Update main.go

--- main.go
+++ main.go
@@ -18,7 +18,6 @@ func helloServer(w http.ResponseWriter, r *http.Request) {
 	if name == "" {
 		name = "world"
 	}
-	fmt.Fprintf(w, fmt.Sprintf("Hello, %s! 0.0.7", name))
 	log.Println(fmt.Sprintf("Hello, %s! 0.0.7", name))
 	
 	w.Header().Set("Content-Type", "application/json")
